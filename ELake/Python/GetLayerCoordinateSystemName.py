c1a628cedcfb695ebbc16167ab5e93b1b1a084df
Нурым Кенжебеков <nkenzhebekov@ecoservice.kz>
2018-10-10 10:28:05 +0600

    gdalwarp

--- ELake/Python/GetLayerCoordinateSystemName.py
+++ ELake/Python/GetLayerCoordinateSystemName.py
@@ -2,7 +2,8 @@ try:
         import gdal
         import osr
 ##        file = raw_input()
-        file = "D:\\GeoServer 2.11.1\\data_dir\\data\\ELake\\Upload\\test4326.tif"
+##        file = "D:\\GeoServer 2.11.1\\data_dir\\data\\ELake\\Upload\\test4326.tif"
+        file = "E:\\Documents\\New\\pastALA.shp"
         ds = gdal.Open(file)
 ##        print osr.SpatialReference(wkt=ds.GetProjection()).GetAttrValue("AUTHORITY", 0) + ":" + osr.SpatialReference(wkt=ds.GetProjection()).GetAttrValue("AUTHORITY", 1)
         print osr.SpatialReference(wkt=ds.GetProjection())
