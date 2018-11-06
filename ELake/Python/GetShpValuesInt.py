35fdc4b6cf31aa759902304941cc9347ee171574
Нурым Кенжебеков <nkenzhebekov@ecoservice.kz>
2018-11-06 16:08:40 +0600

    Python

--- ELake/Python/GetShpValuesInt.py
+++ ELake/Python/GetShpValuesInt.py
@@ -1,9 +1,9 @@
 import shapefile
 
-file=r'C:\Program Files (x86)\GeoServer 2.11.1\data_dir\data\ELake\Lakes\Lakes.shp'
-field = 'id'
-##file = raw_input()
-##field = raw_input()
+##file=r'C:\Program Files (x86)\GeoServer 2.11.1\data_dir\data\ELake\Lakes\Lakes.shp'
+##field = 'id'
+file = raw_input()
+field = raw_input()
 
 sf = shapefile.Reader(file)
 fieldIndex = 1
